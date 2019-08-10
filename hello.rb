10718f7dd1f6858132c2dde2e549f310da784784
JBone Krause <jrkrause81@gmail.com>
2019-08-10 10:26:45 -0500

    First Ruby Commit

---
+++ hello.rb
@@ -0,0 +1,13 @@
+puts "Hello Justin"
+open .
+defaults write com.apple.Finder AppleShowAllFiles YES
+defaults write com.apple.finder AppleShowAllFiles YES
+
+killall Finder
+defaul
+
+
+
+adfl;ja;djfl;ak
+say hi
+]
